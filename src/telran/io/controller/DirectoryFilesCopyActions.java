0530be197068e160a4ead73314bf940758d2a82e
vorabkin <vorabkin@gmail.com>
2022-02-10 11:39:07 +0200

    add Vladimir's hw38 review

--- src/telran/io/controller/DirectoryFilesCopyActions.java
+++ src/telran/io/controller/DirectoryFilesCopyActions.java
@@ -17,6 +17,7 @@ private DirectoryFilesCopyActions() {
 }
 	public static  ArrayList<Item> getItems(DirectiryFilesCopy dfc){
 		DirectoryFilesCopyActions.dfc = dfc;
+		// V.R. This check is redundant. The method is called only once, so...
 		if(items == null) {
 			items = new ArrayList<>();
 			items.add(Item.of("Print directory Tree", DirectoryFilesCopyActions::printDirectoryTree));
@@ -26,15 +27,20 @@ private DirectoryFilesCopyActions() {
 		return items;
 	}
 	private static void printDirectoryTree(InputOutput io) {
+		// V.R. The maxDepth cannot be less than 1. It also cannot be to big 
 		int maxDepth = io.readInt("Enter depth");
+		// V.R. The path has to exist. It is necessary to check existing
 		String path = io.readString("Enter path to directory");
 		dfc.displayDirectoryContent(path, maxDepth, io);
 	}
 	private static void copyFile(InputOutput io) {
 		try {
 			Set<String> flags = new HashSet<>(Arrays.asList("yes","no"));
+			// V.R. "Yes" has to be tested deeper
 			boolean isOverwritten = io.readStringOption("overwritten file? "+ flags, flags).equals("yes")? true: false;
+			// V.R. from address exists?
 			String fromAdres = io.readString("Write adres and file name for target file ");
+			// V.R. If isOverwritten==true, then toAdres has to exist
 			String toAdres = io.readString( "Write adres and file name for destination file");
 			io.writeObjectLine("The speed of coppiing is "+ dfc.copyFiles(fromAdres,toAdres, isOverwritten) +" bites/millisecond");
 		} catch (IOException e) {
--- src/telran/io/service/DirectoryFilesCopyImpl.java
+++ src/telran/io/service/DirectoryFilesCopyImpl.java
@@ -16,18 +16,24 @@ public class DirectoryFilesCopyImpl implements DirectiryFilesCopy {
 	private static final int BOOFER_SIZE = 16*1024*1024;
 
 	InputOutput io;
-
+	// V.R. maxDepth is real data member. As io.
+	// int maxDepth;
 	@Override
 	public void displayDirectoryContent(String directoryPath, int maxDepth, InputOutput io) {
+		// V.R. This check has to be done earlier
 		if (maxDepth <= 0) {
 			maxDepth = Integer.MAX_VALUE;
 		}
 		this.io = io;
+		// V.R. this.maxDepth = maxDepth;
+		
 		File currentDirrectory = new File(directoryPath);
+		// V.R. This check has to be done earlier
 		if (!currentDirrectory.exists()) {
 			io.writeObjectLine("Current dirrectory did't exists");
 			return;
 		}
+		// V.R. This check has to be done earlier
 		if (!currentDirrectory.isDirectory()) {
 			io.writeObjectLine("This is no directory");
 			return;
@@ -38,9 +44,14 @@ public class DirectoryFilesCopyImpl implements DirectiryFilesCopy {
 	}
 
 	private void printDir(File currentDirrectory, int level, int maxDepth) {
+		// V.R. The variable spaces is redundant, may be skipped
 		int spases = level ;
 		boolean flIsDir = currentDirrectory.isDirectory();
 		if (!flIsDir) {
+			/* V.R. May be 
+			 * " ".repeat(level);
+			 *  is simplier 
+			 */
 			for (int i = 0; i < spases; i++) {
 				io.writeObject(" ");
 			}
@@ -57,6 +68,7 @@ public class DirectoryFilesCopyImpl implements DirectiryFilesCopy {
 	@Override
 	public long copyFiles(String pathFileSrc, String pathFileDestination, boolean flOwerride) throws IOException {
 		File target = new File(pathFileSrc);
+		// V.R. The checking has to be executed in DirectoryFilesCopyActions
 		if(!target.exists()) {
 			throw new IOException("current file did't exists");
 		}
